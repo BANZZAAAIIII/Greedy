d92668a131743c7ad64b8649de50ddc8fef6b74a
Martinius Feragen <andrmf18@uia.no>
2021-10-12 15:26:22 +0200

    added greedy tsp #1

--- filemanager.py
+++ filemanager.py
@@ -1,12 +1,21 @@
 from typing import List
 import csv
+import os
 
-def get_data() -> List[dict]:
+
+def get_data(norway=False, short=False) -> List[dict]:
 	dataset = []
+	if short:
+		path = "../data/worldcities_short.csv"
+	else:
+		path = "../data/worldcities.csv"
 
-	with open("data/worldcities.csv", encoding='utf-8') as csv_file:
+	with open(path, encoding='utf-8') as csv_file:
 		reader = csv.DictReader(csv_file)
 		for row in reader:
-			dataset.append({"city": row["city"], "lat": float(row["lat"]), "lng": float(row["lng"])})
+			if norway and row["country"] == "Norway":
+				dataset.append({"city": row["city"], "lat": float(row["lat"]), "lng": float(row["lng"])})
+			elif not norway:
+				dataset.append({"city": row["city"], "lat": float(row["lat"]), "lng": float(row["lng"])})
 
 	return dataset
