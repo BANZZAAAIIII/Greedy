53812a7627fc8a47efed07806542f89d00d07b81
Martinius Feragen <andrmf18@uia.no>
2021-10-17 13:45:45 +0200

    added kruskals algo

--- Minimum spanning tree/minimum_spanning_tree.py
+++ Minimum spanning tree/minimum_spanning_tree.py
@@ -1,6 +1,7 @@
 import math
 import util
-import filemanager as fm
+import heapq as pq
+
 
 def prims_algorithm(graph):
 	n = len(graph)
@@ -36,7 +37,43 @@ def prims_algorithm(graph):
 	print(f"Total cost: {total_cost}")
 
 
+def kruskals_algorithm(graph):
+	F = []  # Represents the minimal spanning tree
+	edges, N = graph
+
+	# Creates a disjointed datastructures. Represents as all vertices being in there own set
+	disjoint_set = util.DisjointSet()
+	disjoint_set.createSet(N)
+
+	# Adds list of edges to a priority queue based in weight
+	queue = []
+	for e in edges:
+		pq.heappush(queue, e)
+
+	# Loop until we have a complete minimal spanning tree
+	while len(F) != N - 1:
+		# pops edge with lowest weight
+		(weight, i, j) = pq.heappop(queue)
+
+		# Finds the root if vertex i an j
+		p = disjoint_set.find(i)
+		q = disjoint_set.find(j)
+
+		# If vertices are not in the same set, merge and add to F
+		if p != q:
+			disjoint_set.union(p, q)
+			F.append((weight, i, j))
+
+	total_cost = 0
+	print("\nedges \t weight")
+	for e in F:
+		print(f"{e[1]} - {e[2]} \t w: {e[0]}")
+		total_cost += e[0]
+	print(f"Total cost: {total_cost}")
+
+
 def main():
+	# matrix representation of the graph in the book
 	matrix_book = [
 		[0, 1, 3, math.inf, math.inf],
 		[1, 0, 3, 8, math.inf],
@@ -44,11 +81,21 @@ def main():
 		[math.inf, 6, 4, 0, 5],
 		[math.inf, math.inf, 2, 5, 0]
 	]
-	# prims_algorithm(matrix_book)
+	prims_algorithm(matrix_book)
 
-	matrix = util.create_matrix(fm.get_data(False, True))
-	prims_algorithm(matrix)
+	# matrix = util.create_matrix(fm.get_data(False, True))
+	# prims_algorithm(matrix)
+
+	# Contains the edges to the graph from the book and the number of vertices in the graph
+	# edges: v_1, v_2, weight
+	graph_book = ([
+		(1, 1, 2), (3, 1, 3), (1, 2, 1), (3, 2, 3), (6, 2, 4), (3, 3, 1), (3, 3, 2), (4, 3, 4), (2, 3, 5), (6, 4, 2),
+		(4, 4, 3), (5, 4, 5), (2, 5, 3), (5, 5, 4)
+	], 5)
+
+	kruskals_algorithm(graph_book)
 
 
 if __name__ == '__main__':
 	main()
+
