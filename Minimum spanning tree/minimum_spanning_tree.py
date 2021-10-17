8837d4d4ee89b32f69a67fb59f923db17f06950c
Martinius Feragen <andrmf18@uia.no>
2021-10-17 11:32:58 +0200

    improved prims

--- Minimum spanning tree/minimum_spanning_tree.py
+++ Minimum spanning tree/minimum_spanning_tree.py
@@ -7,11 +7,8 @@ def prims_algorithm(graph):
 
 	# distance represent the cost to reach a vertex at index. Cannot reach vertex if cost is inf
 	distance = [math.inf for _ in range(0, n)]
-	# represents path taken
 	nearest = [None for _ in range(0, n)]
-
 	distance[0] = 0
-	visited = []
 	nearest[0] = 0
 
 	total_cost = 0
@@ -22,34 +19,24 @@ def prims_algorithm(graph):
 
 		# Finds index witch has the lowest cost and is not visited, saved in edage
 		for i in range(n):
-			if 0 <= distance[i] < min_cost and i not in visited:
+			if 0 <= distance[i] < min_cost:
 				min_cost = distance[i]
 				edge = i
-		visited.append(edge)
-
-		# Adds cost to distance for vertices that are now reachable
+		distance[edge] = -1  # This marks the edge/index as visited
+		nearest[_] = edge
+		# Adds cost to distance for vertices that are now reachable from edge
 		for i in range(n):
-			if 0 <= graph[edge][i] < distance[i] and i not in visited:
+			if graph[edge][i] < distance[i]:
 				distance[i] = graph[edge][i]
-				nearest[i] = edge
 		total_cost += min_cost
 
-	print("Edge \tWeight")
-	for i in range(1, n):
-		print(f"{nearest[i]} - {i} \t {graph[i][nearest[i]]}")
+	# print("Path")
+	# for i in range(1, n):
+	# 	print(f"{nearest[i]}")
 	print(f"Total cost: {total_cost}")
 
 
 def main():
-	"""
-	Using the same matrix given in the book we expect and get the result:
-	Edge 	Weight
-	0 - 1 	 1
-	0 - 2 	 3
-	2 - 3 	 4
-	2 - 4 	 2
-	Total cost: 10
-	"""
 	matrix_book = [
 		[0, 1, 3, math.inf, math.inf],
 		[1, 0, 3, 8, math.inf],
