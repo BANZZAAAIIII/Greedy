2c8c9fbbd4fc0dff65c52099e4d84b1ef13b7a63
Martinius Feragen <Andrmf18@uia.no>
2021-10-18 11:46:47 +0200

    improved tsp greedy algo

--- Travelling Salesperson/tsp.py
+++ Travelling Salesperson/tsp.py
@@ -8,32 +8,27 @@ import filemanager as fm
 
 def tsp_greedy(matrix):
 	n = len(matrix)
-	cost = 0	     # The current total cost of path
-	counter = 1      # Position or how far we have traversed
-	col, row = 0, 0  # Position in matrix
-	min_cost = math.inf  # Local minimum cost
+	total_cost = 0    # The current total cost of path
+	current_node = 0  # Index for row in the matrix, represented as the current node
 
-	path = [-1 for _ in range(0, n)]   # path taken by index, and visited nodes
-	path[0] = 0
+	path = [-1 for _ in range(0, n)]   # Path taken by index, and visited nodes
+	path[0] = current_node             # Adds starting node to first index
 
-	# Loops until all but the path back to start are done
-	while counter < n:
-		if col not in path and col != row:
-			if matrix[row][col] < min_cost:
-				min_cost = matrix[row][col]
-				path[counter] = col
+	for i in range(1, n):
+		min_cost = math.inf  # Local minimum cost
 
-		col += 1
+		# Check all possible routs from this node
+		for next_node in range(n):
+			if next_node not in path and next_node != current_node:
+				if matrix[current_node][next_node] < min_cost:
+					min_cost = matrix[current_node][next_node]
+					path[i] = next_node
 
-		# All possible path from this node is checked
-		if col == n:
-			cost += min_cost     # Adds cost from the path with min cost
-			min_cost = math.inf
-			col = 0
-			row = path[counter]  # next node is from the node
-			counter += 1
+		# Setting up for next loop as all possible path from this node, row, is checked
+		total_cost += min_cost  # Adds cost from the path with min cost
+		current_node = path[i]  # Next node we look at is the one we found with min cost
 
-	return cost, path
+	return total_cost, path
 
 
 def create_matrix(dataset):
@@ -56,8 +51,9 @@ def main():
 	dataset = fm.get_data(False, True)
 	matrix = create_matrix(dataset)
 	cost, route = tsp_greedy(matrix)
-	print(f"min cost: {cost}")
 	print(f"route: {route}")
+	print(f"min cost: {cost}")
+
 
 
 if __name__ == '__main__':
