bb8716b291ca31be377c3b7bd3e32f2aa0175485
Martinius Feragen <andrmf18@uia.no>
2021-10-19 13:21:44 +0200

    added dynamic programming sol to tsp

--- Travelling Salesperson/tsp.py
+++ Travelling Salesperson/tsp.py
@@ -1,56 +1,70 @@
 import math
-from typing import List, Optional
 import util
-import random
 
 import filemanager as fm
 
 
 def tsp_dynamic(matrix):
-	def tsp(start, set):
-		if graph[start][set] != -1:
-			return graph[start][set]
+	def travel(current_v, subset):
+		if not subset:  # if subset is empty
+			return matrix[current_v][0]
 
-		for i in range(set):
-			current_cost = matrix[start][i]
-			other_cost = tsp(i, i)
+		min_cost = math.inf
+		index = None
+		for next_v in subset:
+			# matrix[current_v][next_v] gets cost from the current vertex to the next vertex
+			# travel(next_v, subset.difference({next_v})) recursively get the best cost from the next vertex to subset excluding that vertex
+			cost = matrix[current_v][next_v] + travel(next_v, subset.difference({next_v}))
+			if cost < min_cost:
+				min_cost = cost
+				index = next_v
+		# Save what vertex has the lowest cost from this (vertex, subset) combination
+		p[(current_v, subset)] = index
+		return min_cost
 
-			total_cost = current_cost + other_cost
+	# Creates a immutable set containing all indexes from 1 to length of matrix
+	s = frozenset(range(1, len(matrix)))
+	# Dict that saves best cost from vertex in subset where:
+	# Key:(vertex, set), and value: index, where the index
+	p = {}
 
-		return total_cost
+	# Gets the minimal cost of traversing the adjacency matrix
+	minimal_cost = travel(0, s)
 
-	size = len(matrix)
-	graph = [[-1 for _ in range(0, size+1)] for _ in range(0, size+1)]
-	path = [[-1 for _ in range(0, size+1)] for _ in range(0, size+1)]
+	# We need to look though p to find the optimal path
+	n = 0
+	optimal_path = [n]
+	while s:
+		n = p[(n, s)]           # From current_v vertex and set we get the index to the vertex with lowest cost
+		optimal_path.append(n)  # Append this vertex to the optimal path
+		s = s.difference({n})   # And remove it from the set
 
-	for i in range(size):
-		graph[i][0] = matrix[i][0]
-
-	print(tsp(0, size))
+	return minimal_cost, optimal_path
 
 
 def tsp_greedy(matrix):
 	n = len(matrix)
-	total_cost = 0    # The current total cost of path
-	current_node = 0  # Index for row in the matrix, represented as the current node
+	total_cost = 0  # The current total cost of path
+	current_v = 0   # Index for row in the matrix, represented as the current node
 
-	path = [-1 for _ in range(0, n)]   # Path taken by index, and visited nodes
-	path[0] = current_node             # Adds starting node to first index
+	path = [-1 for _ in range(0, n)]  # Path taken by index, and visited nodes
+	path[0] = current_v               # Adds starting node to first index
 
 	for i in range(1, n):
 		min_cost = math.inf  # Local minimum cost
 
 		# Check all possible routs from this node
-		for next_node in range(n):
-			if next_node not in path and next_node != current_node:
-				if matrix[current_node][next_node] < min_cost:
-					min_cost = matrix[current_node][next_node]
-					path[i] = next_node
+		for next_v in range(n):
+			if next_v not in path and next_v != current_v:
+				if matrix[current_v][next_v] < min_cost:
+					min_cost = matrix[current_v][next_v]
+					path[i] = next_v
 
-		# Setting up for next loop as all possible path from this node, row, is checked
+		# Setting up for next_v loop as all possible path from this node, row, is checked
 		total_cost += min_cost  # Adds cost from the path with min cost
-		current_node = path[i]  # Next node we look at is the one we found with min cost
-
+		current_v = path[i]     # Next node we look at is the one we found with min cost
+	# Adds cost to traversing back to start
+	total_cost += matrix[path[-1]][0]
 	return total_cost, path
 
 
@@ -71,14 +85,23 @@ def create_matrix(dataset):
 
 
 def main():
-	dataset = fm.get_data(False, True)
-	matrix = create_matrix(dataset)
-	# cost, route = tsp_greedy(matrix)
-	# print(f"route: {route}")
-	# print(f"min cost: {cost}")
+	dataset = fm.get_data(True, False)
+	matrix_long = create_matrix(dataset)
+	matrix_book = [
+		[0, 2, 9, math.inf],
+		[1, 0, 6, 4],
+		[math.inf, 7, 0, 8],
+		[6, 3, math.inf, 0]
+	]
+	cost, route = tsp_greedy(matrix_long)
+	print("TSP Greedy algorithm")
+	print(f"\troute: {route}")
+	print(f"\tmin cost: {cost}")
 
-	tsp_dynamic(matrix)
-
+	# print("TSP Dynamic algorithm")
+	# cost2, route2 = tsp_dynamic(matrix)
+	# print(f"\troute: {route2}")
+	# print(f"\tmin cost: {cost2}")
 
 
 if __name__ == '__main__':
